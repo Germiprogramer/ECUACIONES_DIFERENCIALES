856c82f9b47649be5028b25782e5707b6b2b8b7d
Germiprogramer <llorentemunozgerman@gmail.com>
2022-10-13 18:13:45 +0100

    probando

---
+++ ecuaciones/ecuacion1.py
@@ -0,0 +1,30 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy 
+from scipy import integrate
+
+sympy.init_printing(use_latex='mathjax')
+
+x = sympy.Symbol('x')
+y = sympy.Function('y')
+
+ics = {y(3): -1}
+
+f = (x**2*(y(x))-(y(x)))/((y(x))+1)
+print("Ecuación1: " ,sympy.Eq(y(x).diff(x), f))
+
+ec1 = sympy.Eq(y(x).diff(x), f)
+
+#print(ec1)
+
+#C_eq = sympy.Eq(ec1.lhs.subs(x, 3).subs(ics), ec1.rhs.subs(x, 3))
+#print(C_eq)
+
+#print(sympy.solve(C_eq))
+#respuesta
+
+print("Respuesta: ", sympy.dsolve(y(x).diff(x) - f))
+
+
+
